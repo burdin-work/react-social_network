97df6f4a81bc0356a30628db58dffc3d57481f7b
Aleksandr Burdin <burdin.work@gmail.com>
2020-05-29 13:01:10 +0300

    Starting: breakdown. Created main components.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from  'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/40/18/19/4018196360d9fd3324ceffbcc182bcc8.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
