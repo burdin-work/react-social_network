3c0d010fb89f215b9964778e043a3d175c51298c
burdin-work <burdin.work@gmail.com>
2020-08-27 17:08:00 +0300

    Updates

---
+++ precache-manifest.0a23d8f4d6165dc53ffa98c9110c7480.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43637af5ac6076aec62bdcf4412b9a83",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "5de732342934fd3d392c",
+    "url": "/social_network/static/css/3.23685330.chunk.css"
+  },
+  {
+    "revision": "844b652bb49ad0e43734",
+    "url": "/social_network/static/css/main.d7e46287.chunk.css"
+  },
+  {
+    "revision": "0069b94535dfdc14eb52",
+    "url": "/social_network/static/js/2.7b3b4fa2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social_network/static/js/2.7b3b4fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de732342934fd3d392c",
+    "url": "/social_network/static/js/3.46e47090.chunk.js"
+  },
+  {
+    "revision": "844b652bb49ad0e43734",
+    "url": "/social_network/static/js/main.e3af1f2c.chunk.js"
+  },
+  {
+    "revision": "ed537afd016002f19188",
+    "url": "/social_network/static/js/runtime-main.6136f7b5.js"
+  },
+  {
+    "revision": "8cde42b1b1d3e289d115ce90e9742a36",
+    "url": "/social_network/static/media/preloader.8cde42b1.svg"
+  }
+]);
